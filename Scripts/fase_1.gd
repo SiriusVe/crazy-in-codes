e3e5774ccc034be9cadacb4bb97246352e0df50b
SiriusVe <sirius.vessicari@gmail.com>
2026-09-26 16:23:08 -0300

    Atualização Script fase1

--- Scripts/fase_1.gd
+++ Scripts/fase_1.gd
@@ -8,8 +8,16 @@ extends Control
 @onready var c: Button = $QuizUI/QuestionPanel/Answers/C
 @onready var d: Button = $QuizUI/QuestionPanel/Answers/D
 
-var questao_atual = 0
+@onready var mental_bar: ProgressBar = $QuizUI/MentalBar
+@onready var timer_txt: Label = $QuizUI/QuestionPanel/Timertxt
 
+
+var mental = 100.00
+var questao_atual = 0
+var time = 20.0
+var timeR = 20.0
+var error = 10.0
+var fim_questao = false
 
 var questoes = [
 	{
@@ -24,6 +32,7 @@ var questoes = [
 		],
 		"correta": 2
 	},
+	
 	{
 		"linguagem": "Python",
 		"pergunta": "Qual será a saída deste código?",
@@ -39,6 +48,7 @@ var questoes = [
 ]
 
 func mostrar_questao():
+	timeR = time
 	var questao = questoes[questao_atual]
 	
 	question_txt.text = questao["pergunta"]
@@ -48,6 +58,62 @@ func mostrar_questao():
 	b.text =  questao["alternativas"][1]
 	c.text =  questao["alternativas"][2]
 	d.text =  questao["alternativas"][3]
-	 
+	
+	timer_txt.text = str(ceil(timeR))
+
+func verificar_resposta(resposta: int):
+	if fim_questao:
+		return
+	
+	fim_questao = true
+	
+	var questao = questoes[questao_atual]
+	if resposta == questao["correta"]:
+		print("Resposta Correta")
+	else:
+		print("Resposta Incorreta")
+		mental -= error
+		mental = max(mental, 0.0)
+		mental_bar.value = mental
+	
+	proxima_questao()
+	
+func proxima_questao():
+	questao_atual += 1
+	
+	if questao_atual >= questoes.size():
+		print("Fim questoes")
+		return
+	
+	fim_questao = false
+	mostrar_questao()
+	
+	print("Nova questão! Timer:", timeR)
+	
 func _ready():
 	mostrar_questao()
+	a.pressed.connect(func(): verificar_resposta(0))
+	b.pressed.connect(func(): verificar_resposta(1))
+	c.pressed.connect(func(): verificar_resposta(2))
+	d.pressed.connect(func(): verificar_resposta(3))
+
+
+func _process(delta):
+	timeR -= delta
+	timer_txt.text = str(ceil(timeR))
+	mental -= (100.0 / time) * delta
+	mental = max(mental, 0.0)
+	mental_bar.value = mental
+	
+	if timeR <= 0.0 and not fim_questao:
+		timeR = 0.0
+		fim_questao = true
+		
+		print("Tempo esgotado!")
+		
+		mental -= error
+		mental = max(mental, 0.0)
+		mental_bar.value = mental
+		
+		proxima_questao()
+	
